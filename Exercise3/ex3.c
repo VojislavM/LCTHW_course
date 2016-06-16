fb1f74e7c2f91d82d3138c3059effac2d1dcee39
voja <milivojevicvoja@yahoo.com>
2016-06-16 21:04:06 +0200

    ex4

---
+++ Exercise3/ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int age = 10;
+	int height = 72;
+	char string[] = "Voja";
+
+	printf("I am %d years old.\n", age);
+	printf("I am %d inches tall.\n", height);
+	printf("\tString %s\n\r", string);
+
+	return 0;
+}
